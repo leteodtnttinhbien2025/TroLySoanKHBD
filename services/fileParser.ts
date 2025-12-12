64b27aa2cc8d7da099d68ec8c5fa1746cf6fcb03
leteodtnttinhbien2025 <leteodtnttinhbien.agu@gmail.com>
2025-12-12 11:56:32 +0700

    Fix import statement formatting in fileParser.ts

--- services/fileParser.ts
+++ services/fileParser.ts
@@ -1,4 +1,4 @@
-import * as pdfjsLib from 'pdfjs-dist';
+import * as pdfjsLib from 'pdfjs-dist'; 
 import * as mammoth from 'mammoth';
 
 // Khởi tạo Worker cho PDF.js, quan trọng để tránh lỗi trong môi trường trình duyệt.
